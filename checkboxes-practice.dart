4bbd6ad749586dfdf48a52ced99b54c1768ae5a3
durvesh <thes86088@gmail.com>
2026-08-18 23:26:56 +0530

    add AttendanceCard widget

--- checkboxes-practice.dart
+++ checkboxes-practice.dart
@@ -16,51 +16,92 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ThisWidgetHasData extends StatefulWidget{
-    //final void Function( String ) funcToUpdateAttendanceValue ; 
-  
+class ThisWidgetHasData extends StatefulWidget {
+  //final void Function( String ) funcToUpdateAttendanceValue ;
+
   //ThisWidgetHasData( { required this.funcToUpdateAttendanceValue } );
-  
+
   //List<bool>
   State<ThisWidgetHasData> createState() => _ThisWidgetHasDataState();
-  
 }
 
-class _ThisWidgetHasDataState extends State<ThisWidgetHasData>{
-  
-  //List<bool> attendanceToday = [false, false, false, false];  
-  Map<String, bool> attendanceToday = { "01" : false, "02" : false, "03" : false, "04" : false}; 
-  
-  void funcToToggleAttendanceValue( String requiredKey ){
-    setState( (){ attendanceToday[ requiredKey ] = !(attendanceToday[ requiredKey ]!) ; } );
+class _ThisWidgetHasDataState extends State<ThisWidgetHasData> {
+  //List<bool> attendanceToday = [false, false, false, false];
+  Map<String, bool> attendanceToday = {
+    "01": false,
+    "02": false,
+    "03": false,
+    "04": false,
   };
-  
-  Widget build(BuildContext context){
-    return Column(children :[Text( "this page displays the attendance information" ), ThisWidgetHasCheckBoxes( receivedAttendance : attendanceToday, copyOfReceivedAttendance : attendanceToday, funcToToggleAttendanceValue : funcToToggleAttendanceValue ) ]);
+
+  void funcToToggleAttendanceValue(String requiredKey) {
+    setState(() {
+      attendanceToday[requiredKey] = !(attendanceToday[requiredKey]!);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("this page displays the attendance information"),
+        ThisWidgetHasCheckBoxes(
+          receivedAttendance: attendanceToday,
+          copyOfReceivedAttendance: attendanceToday,
+          funcToToggleAttendanceValue: funcToToggleAttendanceValue,
+        ),
+      ],
+    );
   }
 }
 
-class ThisWidgetHasCheckBoxes extends StatelessWidget{
-  
+class ThisWidgetHasCheckBoxes extends StatelessWidget {
   //final List<bool> receivedAttendance;
   //final List<bool> copyOfReceivedAttendance;
   final Map<String, bool> receivedAttendance;
   final Map<String, bool> copyOfReceivedAttendance;
-  final void Function( String ) funcToToggleAttendanceValue ; 
-  
-  ThisWidgetHasCheckBoxes( { required this.receivedAttendance, required this.copyOfReceivedAttendance, required this.funcToToggleAttendanceValue } );
-  
-  Widget build(BuildContext context){
-    
-    List<String> listOfKeys = (receivedAttendance.keys).toList() ;
-    
-    return ListView.builder( itemBuilder : ( context, index ) => AttendanceCard( rollNo : listOfKeys[index], attendanceValue : receivedAttendance[ listOfKeys[index] ], funcToUpdateAttendanceValue : funcToToggleAttendanceValue ) );
+  final void Function(String) funcToToggleAttendanceValue;
+
+  ThisWidgetHasCheckBoxes({
+    required this.receivedAttendance,
+    required this.copyOfReceivedAttendance,
+    required this.funcToToggleAttendanceValue,
+  });
+
+  Widget build(BuildContext context) {
+    List<String> listOfKeys = (receivedAttendance.keys).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => AttendanceCard(
+        rollNo: listOfKeys[index],
+        attendanceValue: (receivedAttendance[listOfKeys[index]]!),
+        funcToToggleAttendanceValue: funcToToggleAttendanceValue,
+      ),
+    );
   }
-  
 }
 
-/*
-class AttendanceCard extends StatelessWidget{
-  
+class AttendanceCard extends StatelessWidget {
+  final String rollNo;
+  final bool attendanceValue;
+  final void Function(String) funcToToggleAttendanceValue;
+
+  AttendanceCard({
+    required this.rollNo,
+    required this.attendanceValue,
+    required this.funcToToggleAttendanceValue,
+  });
+
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Checkbox(
+          value: attendanceValue,
+          onChanged: (bool? value) {
+            funcToToggleAttendanceValue(rollNo);
+          },
+        ),
+        title: Text(rollNo),
+      ),
+    );
+  }
 }
-*/
